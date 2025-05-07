4500b31788b711237ef50f7de8f26a4812b3054e
Edge-5474 <edglives2000years@gmail.com>
2025-05-06 18:09:00 -0600

    few changes

--- schema.sql
+++ schema.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 -- Create unique index for committee_name when is_committee_chair is 1
-CREATE UNIQUE INDEX unique_committee_chair ON users (committee_name) WHERE is_committee_chair = 1;
+-- CREATE UNIQUE INDEX unique_committee_chair ON users (committee_name) WHERE is_committee_chair = 1;
 
 -- Sessions table
 CREATE TABLE IF NOT EXISTS sessions (
@@ -110,16 +110,31 @@ CREATE TABLE IF NOT EXISTS agendas_session(
 );
 
 
-SELECT * FROM sessions;
-SELECT * FROM attendance;
+SELECT * FROM users;
 
-INSERT INTO users (name, username, password_hash, role_name, committee_name, is_admin, is_active)
-VALUES (
-    'Senate Secretary',
-    'secretary',
-    '$2b$10$6vmI3r/ynPnolIwWCO82C.Kl5xVpqBv1/ep7CELl4YXFvGASLN4Ea',
-    'Secretary',
-    'Executive Council',
-    1,
-    1
+SELECT * FROM sessions;
+
+-- INSERT INTO users (name, username, password_hash, role_name, committee_name, is_admin, is_active)
+-- VALUES (
+--     'Senate Secretary',
+--     'secretary',
+--     '$2b$10$6vmI3r/ynPnolIwWCO82C.Kl5xVpqBv1/ep7CELl4YXFvGASLN4Ea',
+--     'Secretary',
+--     'Executive Council',
+--     1,
+--     1
+-- );
+
+CREATE TABLE IF NOT EXISTS sessions (
+    session_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    datetime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_by INTEGER ,
+    status TEXT DEFAULT 'ongoing',
+    FOREIGN KEY (created_by) REFERENCES users(user_id)
 );
+
+-- INSERT INTO sessions (session_id,created_by)
+-- VALUES (
+--     1,
+--     1
+-- );
